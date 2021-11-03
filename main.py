0645a03d02dcd3af1c65cb4f5bc1c016214a4b6d
mark <67511608+Marusiella@users.noreply.github.com>
2021-11-03 20:11:19 +0100

    szybka zmiana

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ while True:
     dodawanie = jakas_tan("+", inx)
     odejmowanie = jakas_tan("-", inx)
     mnozenie = jakas_tan("*", inx)
-    dzielenie = jakas_tan(":", inx)
+    dzielenie_dwu = jakas_tan(":", inx)
+    dzielenie_ukos = jakas_tan("/", inx)
     if dodawanie != False:
         print("dodawanie")
         print(dodawanie[0]+dodawanie[1])
@@ -25,6 +26,9 @@ while True:
     if mnozenie != False:
         print("mnozenie")
         print(mnozenie[0]*mnozenie[1])
-    if dzielenie != False:
-        print("dzielenie")
-        print(dzielenie[0]/dzielenie[1])
+    if dzielenie_dwu != False:
+        print("Dzielenie poprzez użycie :")
+        print(dzielenie_dwu[0]/dzielenie_dwu[1])
+    if dzielenie_ukos != False:
+        print("Dzielenie poprzez użycie /")
+        print(dzielenie_ukos[0]/dzielenie_ukos[1])
